0e238975a2f8c86bd973ce1f0b51f5d907be279b
metallica <cristianmanuel007@gmail.com>
2024-01-15 22:20:22 -0400

    fix global.lua(error create file undo)

--- lua/config/global.lua
+++ lua/config/global.lua
@@ -64,7 +64,7 @@ vim.api.nvim_exec([[
 if vim.fn.has("persistent_undo") == 1 then
     local target_path = vim.fn.expand('~/.local/share/nvim/undodir')
     if vim.fn.isdirectory(target_path) == 0 then
-        vim.fn.mkdir(target_path, "p", 0700)
+        vim.fn.mkdir(target_path, "p")
     end
     vim.o.undodir = target_path
     vim.o.undofile = true
